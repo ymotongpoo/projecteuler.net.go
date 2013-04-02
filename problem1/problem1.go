9b12f15c5f1c84ba5d5c14ceb1cd5a979d8b2061
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2013-04-02 18:50:42 +0900

    solved problem1

---
+++ problem1/problem1.go
@@ -0,0 +1,23 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5,
+// we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Find the sum of all the multiples of 3 or 5 below 1000.
+package main
+
+import (
+	"fmt"
+)
+
+func SumMultiplesBelow(n int) int {
+	sum := 0
+	for i:=0; i<n; i++ {
+		if i % 3 == 0 || i % 5 == 0 {
+			sum += i
+		}
+	}
+	return sum
+}
+
+func main() {
+	fmt.Println(SumMultiplesBelow(1000))
+}
