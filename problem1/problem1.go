c325db3eeb62e4020b7516ad4ae4773cf55c022c
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2013-04-02 18:50:55 +0900

    solved problem1

--- problem1/problem1.go
+++ problem1/problem1.go
@@ -10,8 +10,8 @@ import (
 
 func SumMultiplesBelow(n int) int {
 	sum := 0
-	for i:=0; i<n; i++ {
-		if i % 3 == 0 || i % 5 == 0 {
+	for i := 0; i < n; i++ {
+		if i%3 == 0 || i%5 == 0 {
 			sum += i
 		}
 	}
