bc3ae27e8f0cb5afa91d88adde66f8622eb50a25
Yoshifumi YAMAGUCHI <ymotongpoo@gmail.com>
2015-01-12 17:56:20 +0900

    thinking cyclic num detection method

--- problem0061/problem0061.go
+++ problem0061/problem0061.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func triangle(n int) int {
 	return (n*n + n) / 2
 }
@@ -26,6 +24,34 @@ func octagonal(n int) int {
 	return 3*n*n - 2*n
 }
 
+func findFourDigits(f func(int) int) []int {
+	ret := []int{}
+	i := 0
+	for {
+		n := f(i)
+		if n < 1000 {
+			i++
+			continue
+		} else if n >= 10000 {
+			return ret
+		}
+		ret = append(ret, n)
+		i++
+	}
+}
+
 func main() {
-	fmt.Println("hoge")
+	polygonals := make([][]int, 6)
+	for i := range polygonals {
+		polygonals[i] = []int{}
+	}
+
+	polygonals[0] = findFourDigits(triangle)
+	polygonals[1] = findFourDigits(square)
+	polygonals[2] = findFourDigits(pentagonal)
+	polygonals[3] = findFourDigits(hexagonal)
+	polygonals[4] = findFourDigits(heptagonal)
+	polygonals[5] = findFourDigits(octagonal)
+
+	// TODO(ymotongpoo): implement cyclic number detection algorithm.
 }
