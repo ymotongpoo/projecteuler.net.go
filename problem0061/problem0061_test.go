c760e8d119994608262c01067c5fd3f0be24d815
Yoshifumi YAMAGUCHI <ymotongpoo@gmail.com>
2015-01-12 16:37:44 +0900

    merged test funcs into one

--- problem0061/problem0061_test.go
+++ problem0061/problem0061_test.go
@@ -5,35 +5,42 @@ import (
 	"testing"
 )
 
-func TestTriangle(t *testing.T) {
-	want := []int{1, 3, 6, 10, 15}
+func testPolygonal(t *testing.T, want []int, f func(int) int) {
 	out := make([]int, len(want))
 	for i := range want {
-		out[i] = triangle(i + 1)
+		out[i] = f(i + 1)
 	}
 	if !reflect.DeepEqual(want, out) {
 		t.Errorf("want: %v, out: %v", want, out)
 	}
+}
+
+func TestTriangle(t *testing.T) {
+	want := []int{1, 3, 6, 10, 15}
+	testPolygonal(t, want, triangle)
 }
 
 func TestSquare(t *testing.T) {
 	want := []int{1, 4, 9, 16, 25}
-	out := make([]int, len(want))
-	for i := range want {
-		out[i] = square(i + 1)
-	}
-	if !reflect.DeepEqual(want, out) {
-		t.Errorf("want: %v, out: %v", want, out)
-	}
+	testPolygonal(t, want, square)
 }
 
 func TestPentagonal(t *testing.T) {
 	want := []int{1, 5, 12, 22, 35}
-	out := make([]int, len(want))
-	for i := range want {
-		out[i] = pentagonal(i + 1)
-	}
-	if !reflect.DeepEqual(want, out) {
-		t.Errorf("want: %v, out: %v", want, out)
-	}
+	testPolygonal(t, want, pentagonal)
+}
+
+func TestHexagonal(t *testing.T) {
+	want := []int{1, 6, 15, 28, 45}
+	testPolygonal(t, want, hexagonal)
+}
+
+func TestHeptagonal(t *testing.T) {
+	want := []int{1, 7, 18, 34, 55}
+	testPolygonal(t, want, heptagonal)
+}
+
+func TestOctagonal(t *testing.T) {
+	want := []int{1, 8, 21, 40, 65}
+	testPolygonal(t, want, octagonal)
 }
